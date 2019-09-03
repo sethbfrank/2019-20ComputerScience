92147521b5d2212f0f65445883622121e95f86d0
Seth Frank <21frankse@fpsct.org>
2019-09-03 10:33:10 -0400

    Started stuff with java basics

---
+++ javaBasics/ReviewVariablesAndIfs.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class ReviewVariablesAndIfs
+{
+public static void main()
+{
+//add code in here
+Scanner someScanner = new Scanner(System.in);
+
+System.out.print("Enter quantity: ");
+int quantity = someScanner.nextInt();
+someScanner.nextLine();
+}
+}
