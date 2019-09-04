a6ba5931bb71d1d66abf469acbe981bd986a454e
Seth Frank <21frankse@fpsct.org>
2019-09-04 10:33:18 -0400

    More stuff with variables and if statements

--- javaBasics/ReviewVariablesAndIfs.java
+++ javaBasics/ReviewVariablesAndIfs.java
@@ -2,13 +2,52 @@ import java.util.Scanner;
 
 public class ReviewVariablesAndIfs
 {
-public static void main()
-{
-//add code in here
-Scanner someScanner = new Scanner(System.in);
-
-System.out.print("Enter quantity: ");
-int quantity = someScanner.nextInt();
-someScanner.nextLine();
+    public static void main()
+    {
+    //add code in here
+    //int num; declaration
+    //num = 0; assignment, initialization
+    //num = 1; assignment
+    
+    Scanner someScanner = new Scanner(System.in);
+    
+    System.out.print("What is your age: ");
+    int num = someScanner.nextInt();
+    someScanner.nextLine();
+    System.out.println(num);
+    
+    System.out.print("How many AP's are you taking: ");
+    num = someScanner.nextInt();
+    someScanner.nextLine();
+    //System.out.println(num);
+    
+    if(num <= 3)
+        {
+            System.out.println("Good job!");
+        }else{
+            System.out.println("That's too many!");
+            }
+            
+    System.out.print("Do you play an instrument? (Enter true or false): ");
+    boolean answer = someScanner.nextBoolean();
+    someScanner.nextLine();
+    System.out.println(answer);
+    
+    if(answer == true)
+        {
+            System.out.println("Yeah!");
+        }else{
+            System.out.println("You should play an instrument.");
+        }
+        
+    if(num <= 3)
+    {
+        if(answer == true)
+        {
+            
+        }
+    
+    
+    }
 }
 }
