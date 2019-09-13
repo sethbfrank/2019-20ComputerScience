210b0248c747c73ab98c57e8c0d897a659b1d862
Seth Frank <21frankse@fpsct.org>
2019-09-13 10:42:34 -0400

    Practice with methods

---
+++ javaBasics/Methods.java
@@ -0,0 +1,35 @@
+
+public class Methods
+{
+    // instance variables - replace the example below with your own
+    
+    public static void main()
+    {
+        verse1();
+        chorus();
+        verse2();
+        chorus();
+        chorus();
+    }
+
+    public static void chorus()
+    {
+        System.out.println("And I'm proud to be an American\nWhere at least I know I'm free\nAnd I won't forget the men who died\nWho gave that right to me");
+        System.out.println("And I'd gladly stand up next to you\nAnd defend Her still today\n'Cause there ain't no doubt\nI love this land\nGod Bless the U.S.A.\n");
+        return;
+    }
+    
+    public static void verse1()
+    {
+        System.out.println("\nIf tomorrow all the things were gone\nI worked for all my life\nAnd I had to start again\nWith just my children and my wife\n");
+        System.out.println("I thank my lucky stars\nTo be living here today\n'Cause the flag still stands for freedom\nAnd they can't take that away\n");
+        return;
+    }
+    
+    public static void verse2()
+    {
+        System.out.println("From the lakes of Minnesota\nTo the hills of Tennessee\nAcross the plains of Texas\nFrom sea to shining sea\n");
+        System.out.println("From Detroit down to Houston\nAnd New York to L.A.\nWhere's pride in every American heart\nAnd it's time we stand and say\n");
+        return;
+    }
+}
