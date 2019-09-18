68ac99c0a15dcd704bef80ac6752d08457336935
Seth Frank <21frankse@fpsct.org>
2019-09-18 10:33:11 -0400

    stuff with methods in java api

---
+++ javaBasics/CallingMethods.java
@@ -0,0 +1,12 @@
+
+public class CallingMethods
+{
+       
+    public static void main()
+    {
+        String s1 = new String("Java has a dictionary!");
+        System.out.println(s1.length());
+        System.out.println(s1.toUpperCase());
+        System.out.println(s1.substring(11));       
+    }
+}
