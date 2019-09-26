4086bc5c6630da4aca1f2e8d70500625c12d69e9
Seth Frank <21frankse@fpsct.org>
2019-09-26 10:33:02 -0400

    started the employee class

---
+++ unit1/Employee.java
@@ -0,0 +1,36 @@
+
+/**
+ * Write a description of class Employee here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Employee
+{
+    //fields
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String idNum;
+    private String title;
+    private double salary;
+
+    //constructors
+    public Employee()
+    {
+        // initialise instance variables
+        firstName = "Seth";
+        lastName = "Frank";
+        age = 16;
+        idNum = "1";
+        title = "Secretary";
+        salary = 200.00;
+    }
+
+    //methods
+    public void sampleMethod()
+    {
+        // put your code here
+        
+    }
+}
