0f600a85c1cdc181417a130a116c28bcc0fab4e7
Seth Frank <21frankse@fpsct.org>
2019-09-27 17:24:15 -0400

    Finished formative 1.3 with employee class

--- unit1/Employee.java
+++ unit1/Employee.java
@@ -26,11 +26,79 @@ public class Employee
         title = "Secretary";
         salary = 200.00;
     }
+    
+    public Employee(String myFirstName, String myLastName, int myAge, String myTitle, double mySalary)
+    {
+        firstName = myFirstName;
+        lastName = myLastName;
+        age = myAge;
+        title = myTitle;
+        salary = mySalary;
+        idNum = "00-000-0000";
+    }
 
     //methods
-    public void sampleMethod()
+    public String getFirstName()
     {
-        // put your code here
-        
+        return firstName;        
+    }
+    public String getLastName()
+    {
+        return lastName;
+    }
+    public int getAge()
+    {
+        return age;
+    }
+    public String getIDNum()
+    {
+        return idNum;
+    }
+    public String getTitle()
+    {
+        return title;
+    }
+    public double getSalary()
+    {
+        return salary;
+    }
+    
+    public void setFirstName(String newFirstName)
+    {
+        firstName = newFirstName;
+    }
+    public void setLastName(String newLastName)
+    {
+        lastName = newLastName;
+    }
+    public void setIDNum(String newIDNum)
+    {
+        idNum = newIDNum;
+    }
+    
+    public void increaseAge()
+    {
+        age++;
+    }
+    public void giveRaise(double amount)
+    {
+        salary = salary  + amount;
+    }
+    public void changePosition(String newTitle, double newSalary)
+    {
+        title = newTitle;
+        salary = newSalary;
+    }
+    public void fire()
+    {
+        title = "Terminated";
+        salary = 0;
+    }
+    public void printPersonnelReport()
+    {
+        System.out.println("This employee is " + firstName + " " + lastName + ".");
+        System.out.println("They are " + age + " years old.");
+        System.out.println("Their title is " + title + " and they have a salary of $" + salary);
+        System.out.println("Their ID number is " + idNum);
     }
 }
