3319689aedb0ca77df8f4c84479f6fedb1edfe6b
Seth Frank <21frankse@fpsct.org>
2019-10-17 10:32:17 -0400

    Continued work on flight class

--- unit1/Flight.java
+++ unit1/Flight.java
@@ -1,100 +1,124 @@
-/**
- * This is an example of a flight class.
- * It has information that would be found on a passengers ticket.
- * This could be part of a bigger Airport class
+/*
+ * This is an example of a very simple airport class.
+ * It could be connected to other classes, however, this was made to be very simple.
  * 
  * Author: Seth Frank
  * Version: 1.0
  */
 
- import java.util.Random;
+import java.util.Random;
 
- public class Flight
- {
-    //fields
-    private String airline;
-    private int flightNum;
-    private int gateNum;
-    private String seat;
-    private String departingAirport;
-    private String arrivingAirport;
+public class Flight
+{
+   //fields
+   private String airline;
+   private int flightNum;
+   private int gateNum;
+   private String seat;
+   private String departingAirport;
+   private String arrivingAirport;
 
-    //constructors
-    public Flight()
-    {
-        airline = "Southwest";
-        flightNum = 238;
-        gateNum = 4;
-        seat = "6F";
-        departingAirport = "BDL";
-        arrivingAirport = "MCO";
-    }
+   //constructors
+   public Flight()
+   {
+       airline = "Southwest";
+       flightNum = 238;
+       gateNum = 4;
+       seat = "6F";
+       departingAirport = "BDL";
+       arrivingAirport = "MCO";
+   }
 
-    public Flight(String myAirline, int myFlightNum, int myGateNum, String mySeat, String myDeparture, String myArrival)
-    {
-        airline = myAirline;
-        flightNum = myFlightNum;
-        gateNum = myGateNum;
-        seat = mySeat;
-        departingAirport = myDeparture;
-        arrivingAirport = myArrival;
-    }
+   public Flight(String myAirline, int myFlightNum, int myGateNum, String mySeat, String myDeparture, String myArrival)
+   {
+       airline = myAirline;
+       flightNum = myFlightNum;
+       gateNum = myGateNum;
+       seat = mySeat;
+       departingAirport = myDeparture;
+       arrivingAirport = myArrival;
+   }
 
-    public Flight(String myAirline, String myDeparture, String myArrival)
-    {
-        airline = myAirline;
-        
-        Random randFlightNum = new Random();
-        flightNum = randFlightNum.nextInt(9999);
-        Random randGateNum = new Random();
-        gateNum = randGateNum.nextInt(99);
-        //Random randSeatNum = new Random();
-        //int seatNum = randSeatNum.nextInt(30);
-        seat = "6F";
+   public Flight(String myAirline, String myDeparture, String myArrival)
+   {
+       airline = myAirline;
+       
+       Random randFlightNum = new Random();
+       flightNum = randFlightNum.nextInt(9999);
+       Random randGateNum = new Random();
+       gateNum = randGateNum.nextInt(99);
+       //Random randSeatNum = new Random();
+       //int seatNum = randSeatNum.nextInt(30);
+       seat = "6F";
 
-        departingAirport = myDeparture;
-        arrivingAirport = myArrival;        
-    }
+       departingAirport = myDeparture;
+       arrivingAirport = myArrival;        
+   }
 
-    //accessor methods
-    public String getAirline()
-    {
-        return airline;
-    }
-    public int getFlightNum()
-    {
-        return flightNum;
-    }
-    public int getGateNum()
-    {
-        return gateNum;
-    }
-    public String getSeat()
-    {
-        return seat;
-    }
-    public String getDeparture()
-    {
-        return departingAirport;
-    }
-    public String getArrival()
-    {
-        return arrivingAirport;
-    }
+   //accessor methods
+   public String getAirline()
+   {
+       return airline;
+   }
+   public int getFlightNum()
+   {
+       return flightNum;
+   }
+   public int getGateNum()
+   {
+       return gateNum;
+   }
+   public String getSeat()
+   {
+       return seat;
+   }
+   public String getDeparture()
+   {
+       return departingAirport;
+   }
+   public String getArrival()
+   {
+       return arrivingAirport;
+   }
 
-    //mutator methods
-    public String setDeparture(String newDeparture)
-    {
-        departingAirport = newDeparture;
-        return departingAirport;
-    }
-    public String setArrival(String newArrival)
-    {
-        arrivingAirport = newArrival;
-        return arrivingAirport;
-    }
+   //mutator methods
+   public String setDeparture(String newDeparture)
+   {
+       departingAirport = newDeparture;
+       return departingAirport;
+   }
+   public String setArrival(String newArrival)
+   {
+       arrivingAirport = newArrival;
+       return arrivingAirport;
+   }
 
-    //more complex methods
-    
+   //more complex methods
+   public void flightChange(String newArrival)
+   {
+       Random randFlightNum = new Random();
+       flightNum = randFlightNum.nextInt(9999);
+       Random randGateNum = new Random();
+       gateNum = randGateNum.nextInt(99);
+       arrivingAirport = newArrival;
+   }
+   public void flightCanceled()
+   {
+       airline = "Canceled";
+       flightNum = null;
+       gateNum = null;
+       seat = "Canceled";
+       departingAirport = "Canceled";
+       arrivingAirport = "Canceled";
+   }
 
- }
+   //print method
+   public void printFlightInfo()
+   {
+       System.out.println("You are on " + airline + " flight " + flightNum + ".");
+       System.out.println("You are boarding at gate "+ gateNum + ".");
+       System.out.println("You are sitting in seat " + seat + ".");
+       System.out.println("You are departing from " + departingAirport + " and arriving in " + arrivingAirport + ".");
+   }
+
+}
---
+++ unit1/FlightTester.java
@@ -0,0 +1,83 @@
+/*
+This is the tester for the flight class\
+*/
+
+public class FlightTester
+{
+    public static void main (String[] args)
+    {
+        //Object 1
+        Flight f1 = new Flight();
+        System.out.println(f1);
+
+        //Accessor methods
+        System.out.println(f1.getAirline());
+        System.out.println(f1.getFlightNum());
+        System.out.println(f1.getGateNum());
+        System.out.println(f1.getSeat());
+        System.out.println(f1.getDeparture());
+        System.out.println(f1.getArrival());
+
+        //Mutators
+        f1.setDeparture("JFK");
+        f1.setArrival("BOS");
+        f1.printFlightInfo();
+
+        //More complex methods
+        f1.flightCanceled();
+        f1.printFlightInfo();
+
+        f1.flightChange("LGA");
+        f1.printFlightInfo();
+
+
+        //Object 2
+        Flight f2 = new Flight();
+        System.out.println(f2);
+
+        //Accessor methods
+        System.out.println(f2.getAirline());
+        System.out.println(f2.getFlightNum());
+        System.out.println(f2.getGateNum());
+        System.out.println(f2.getSeat());
+        System.out.println(f2.getDeparture());
+        System.out.println(f2.getArrival());
+
+        //Mutators
+        f2.setDeparture("JFK");
+        f2.setArrival("BOS");
+        f2.printFlightInfo();
+
+        //More complex methods
+        f2.flightCanceled();
+        f2.printFlightInfo();
+        
+        f2.flightChange("LGA");
+        f2.printFlightInfo();
+
+
+        //Object 3
+        Flight f3 = new Flight();
+        System.out.println(f3);
+
+        //Accessor methods
+        System.out.println(f3.getAirline());
+        System.out.println(f3.getFlightNum());
+        System.out.println(f3.getGateNum());
+        System.out.println(f3.getSeat());
+        System.out.println(f3.getDeparture());
+        System.out.println(f3.getArrival());
+
+        //Mutators
+        f3.setDeparture("JFK");
+        f3.setArrival("BOS");
+        f3.printFlightInfo();
+
+        //More complex methods
+        f3.flightCanceled();
+        f3.printFlightInfo();
+        
+        f3.flightChange("LGA");
+        f3.printFlightInfo();
+    }
+}
