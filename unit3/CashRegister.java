c269f09412990e92e49830016e321ed05990a0d2
Seth Frank <21frankse@fpsct.org>
2019-11-20 10:32:43 -0500

    Added printRegisterStatus

--- unit3/CashRegister.java
+++ unit3/CashRegister.java
@@ -24,6 +24,19 @@ public class CashRegister
 
 
     //methods
+    public void printRegisterStatus()
+    {
+        System.out.println(currentID);
+        System.out.println("Twenties: " + twenties);
+        System.out.println("Tens: " + tens);
+        System.out.println("Fives: " + fives);
+        System.out.println("Ones: " + ones);
+        System.out.println("Quarters: " + quarters);
+        System.out.println("Dimes: " + dimes);
+        System.out.println("Nickels: " + nickels);
+        System.out.println("Pennies: " + pennies);
+    }
+    
     public void replenish()
     {
         twenties = 5;
--- unit3/CashRegisterTester.java
+++ unit3/CashRegisterTester.java
@@ -3,6 +3,6 @@ public class CashRegisterTester
     public static void main(String[] args)
     {
         CashRegister r1 = new CashRegister();
-        //System.out.println(r1.replenish());
+        r1.printRegisterStatus();
     }
 }
