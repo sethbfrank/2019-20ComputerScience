5e235bf68a48e7da1112ac073a14504444f75896
Seth Frank <21frankse@fpsct.org>
2019-12-19 10:33:16 -0500

    Started cryptography project

---
+++ unit4/CryptographyProject/Message.java
@@ -0,0 +1,19 @@
+public class Message
+{
+    //fields
+    //private String message;
+    private String plaintext;
+    private String ciphertext;
+    private boolean isEncrypted;
+
+    //constructors
+    public Message(String plaintext)
+    {
+        this.plaintext = plaintext;
+    }
+
+
+    //methods
+
+
+}
