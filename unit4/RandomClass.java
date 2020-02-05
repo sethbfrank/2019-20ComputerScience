a8ad1adefa9ff8fe572756b5b47b0c7054138f19
Seth Frank <21frankse@fpsct.org>
2020-02-05 10:31:25 -0500

    Changed random stuff from the RandomClass

--- unit4/RandomClass.java
+++ unit4/RandomClass.java
@@ -2,6 +2,13 @@ public class RandomClass
 {
     public static void main(String[] args) 
     {
+        int[] numbers = new int[2];
+        System.out.println(numbers[0]);
+        for(int i = 0; i < numbers.length; i++)
+        {
+            numbers[i]++;
+            System.out.println(numbers[i]);
+        }
 
     }        
 }
