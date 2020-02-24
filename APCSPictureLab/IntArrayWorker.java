41c5d048b255a96a2eb48bb88f369b989665f987
Seth Frank <21frankse@fpsct.org>
2020-02-24 10:33:36 -0500

    Continued work with A4

--- APCSPictureLab/IntArrayWorker.java
+++ APCSPictureLab/IntArrayWorker.java
@@ -80,7 +80,24 @@ public class IntArrayWorker
 
   public int getLargest()
   {
-    return 0;
+    int largest = matrix[0][0];
+    for (int[] row : matrix) {
+      for (int position : row) {
+        if(position > largest) {
+          largest = position;
+        }
+      }
+    }
+    return largest;
+  }
+
+  public int getColTotal(int col)
+  {
+    int total = 0;
+    for(int[] row : matrix) {
+      total+=row[col];
+    }
+    return total;
   }
   
   /**
