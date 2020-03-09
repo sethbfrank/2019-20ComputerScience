084156a15ee507acfc473c58772df906ab40f17d
Seth Frank <21frankse@fpsct.org>
2020-03-09 19:32:18 -0400

    Added challenge 2 and 3

--- unit7/Test.java
+++ unit7/Test.java
@@ -2,7 +2,8 @@ public class Test
 {
     public static void main(String[] args)
     {
-
+        Zoo z1 = new Zoo(10);
+        z1.soundOff();
     }
 
     public static void test1()
---
+++ unit7/Zoo.java
@@ -0,0 +1,46 @@
+public class Zoo
+{
+    //fields
+    private Pet[] theZoo;
+
+    //constructors
+    public Zoo()
+    {
+        theZoo = new Pet[10];
+        for(int i = 0; i < 10; i++)
+        {
+            if(i < 5)
+            {
+                theZoo[i] = new Pet();
+            } else {
+                theZoo[i] = new Turkey();
+            }            
+        }
+        
+    }
+
+    public Zoo(int len)
+    {
+        double num;
+        theZoo = new Pet[len];
+        for(int i = 0; i < len; i++)
+        {
+            num = Math.random();
+            if(num < 0.5)
+            {
+                theZoo[i] = new Pet();
+            } else {
+                theZoo[i] = new Turkey();
+            }
+        }
+    }
+
+    //methods
+    public void soundOff()
+    {
+        for(Pet pet : theZoo)
+        {
+            System.out.println(pet.speak());
+        }
+    }
+}
