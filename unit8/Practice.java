93e8444e4b9e5f6999270dfb952695e4d507b404
Seth Frank <21frankse@fpsct.org>
2020-03-31 13:55:59 -0400

    Updated Practice.java

--- unit8/Practice.java
+++ unit8/Practice.java
@@ -3,13 +3,16 @@ import java.util.Arrays;
 
 public class Practice
 {
+    public static int timesTwo(int x)
+    {
+        return x*2;
+    }
+
     public static void main(String[] args)
     {
-        ArrayList<Integer> nums = new ArrayList<Integer>();
-        nums = (ArrayList<Integer>) Arrays.asList(10, 8, 5, 7, 13, 14, 16, 5);
-        for(int num : nums)
-        {
-            System.out.println(num);
-        }
+        Integer val = 10;
+        int result1 = timesTwo(val);
+        Integer result2 = result1;
+        System.out.print(result2);
     }
 }
